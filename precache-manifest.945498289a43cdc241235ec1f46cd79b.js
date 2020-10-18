72df203aad1711f3ca3af48ea4d16b9cf0d2605a
rafael ubuntu <ryikeda@outlook.com>
2020-10-18 18:39:41 +0900

    Updates

---
+++ precache-manifest.945498289a43cdc241235ec1f46cd79b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221ad6400d763cf452379bdb1553cf4c",
+    "url": "/redux-shoply/index.html"
+  },
+  {
+    "revision": "02a0b344fac8e51b389d",
+    "url": "/redux-shoply/static/css/main.f4b11eee.chunk.css"
+  },
+  {
+    "revision": "9779d39511a847bbbb09",
+    "url": "/redux-shoply/static/js/2.62f3c6fa.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/redux-shoply/static/js/2.62f3c6fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a0b344fac8e51b389d",
+    "url": "/redux-shoply/static/js/main.aa499ff5.chunk.js"
+  },
+  {
+    "revision": "079d7bf45a020c39767e",
+    "url": "/redux-shoply/static/js/runtime-main.892d178a.js"
+  }
+]);
